3dd6299b22c3052caa6a5e53fff3febf319f0cee
farhad.sh <farhadshahrabadi@gmail.com>
2022-04-16 20:52:23 +0430

    feature : UserDetail

--- src/components/visit/UserDetail.tsx
+++ src/components/visit/UserDetail.tsx
@@ -1,7 +1,154 @@
-import React from 'react'
+import React from "react";
+import { FormControl } from '@mui/material';
+import { Input , MenuItem , TextField ,InputAdornment} from '@mui/material'
 
-export default function UserDetail() {
+export default function UserDetail(props : any) {
+  // const userDetail = {...props.userDetail}
+
   return (
-    <div>UserDetail</div>
-  )
+    <FormControl>
+      {/* <div>UserDetail</div>
+      <input name="Name" onChange={(e) => props.userDetailHandler(e)} /> */}
+      <TextField 
+          required
+          id="Height"
+          label="قد"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        />
+      <TextField 
+          required
+          id="Weight"
+          label="وزن"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">کیلوگرم</InputAdornment>,
+          }}
+        />
+      <TextField 
+          required
+          id="Height"
+          label="محیط مچ دست غیر فعال"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        />
+
+      <TextField 
+          required
+          id=""
+          label="میزان فعالیت"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        >
+        {/* <MenuItem key={option.value} value={option.value}>
+              {option.label}
+        </MenuItem> */}
+        </TextField>
+      <TextField 
+          required
+          id="Height"
+          label="نبض درحالت استراحت"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+        />
+      <TextField 
+          id="Height"
+          label="محیط دور شکم"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        />
+      <TextField 
+          id="Height"
+          label="محیط دور لگن"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        />
+      <TextField 
+          id="Height"
+          label="فشار خون بالا"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+
+        />
+      <TextField 
+          id="Height"
+          label="فشار خون پایین"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+
+        />
+        
+      <TextField 
+          required
+          id=""
+          label=""
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        />
+        
+      <TextField 
+          required
+          id="Height"
+          label="زمان خواب در شبانه روز"
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">ساعت</InputAdornment>,
+          }}
+        />
+        
+      <TextField 
+          required
+          id=""
+          label=""
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        />
+      <TextField 
+          required
+          id=""
+          label=""
+          type='number'
+          defaultValue={180}
+          onChange={(e) => props.userDetailHandler(e)}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">سانتی متر</InputAdornment>,
+          }}
+        />
+    </FormControl>
+  );
 }
